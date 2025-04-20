615bddacb64a6f0302043e59d33c5565f0f37627
Mashrafie Rahim Sheikh <rahimsheikh05@gmail.com>
2025-04-20 16:27:14 +0600

    feat(auth): implement sign-in endpoint in AuthController and inject AuthService refactor(users): simplify UsersService constructor by removing unused EntityManager

--- src/app/modules/users/users.service.ts
+++ src/app/modules/users/users.service.ts
@@ -1,15 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { UserRepository } from './users.repository';
-import { EntityManager } from '@mikro-orm/postgresql';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/users.entity';
 
 @Injectable()
 export class UsersService {
-  constructor(
-    private readonly userRepository: UserRepository,
-    private readonly em: EntityManager,
-  ) {}
+  constructor(private readonly userRepository: UserRepository) {}
   async getUsers(): Promise<User[]> {
     return this.userRepository.findAll();
   }
